ae64a24335bbd5a988148842f56031dad8873293
fernando-paredes <ferparmur@gmail.com>
2024-09-01 09:50:37 +0100

    made typescript happy

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
+	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
