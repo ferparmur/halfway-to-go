0c0b6f60d3c9405bbfdb8e85a5ec96060e70948b
fernando-paredes <ferparmur@gmail.com>
2024-09-04 16:51:07 +0100

    add external links plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { externalLink } from "./src/externalLink.ts";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [[externalLink, { domain: "fparedes.com" }]],
+	}
 });
