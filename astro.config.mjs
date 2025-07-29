5158bc93eaee205526123aa853ace52199c8d6c1
fernando-paredes <ferparmur@gmail.com>
2025-07-29 01:54:11 +0100

    reformatted mdx with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   site: "https://www.halfwaytogo.com",
   image: {
     responsiveStyles: true,
-    layout: 'constrained',
+    layout: "constrained",
   },
   integrations: [mdx(), sitemap()],
   markdown: {
